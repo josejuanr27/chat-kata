a81e4ccba34862ecbd2bc39d65d5f60d0808346f
JoseJuan <josejuanr27@hotmail.com>
2013-06-25 13:58:16 +0200

    Paso 1 Completado. Interfaz completada

--- client/SimpleChat/src/org/ejmc/android/simplechat/ChatActivity.java
+++ client/SimpleChat/src/org/ejmc/android/simplechat/ChatActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Chat activity.
@@ -13,11 +15,16 @@ import android.view.MenuItem;
  */
 public class ChatActivity extends Activity {
 
+	private TextView ip;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_chat);
+		ip = (TextView) findViewById(R.id.user);
 
+		Bundle bundle = getIntent().getExtras();
+        ip.setText(bundle.getString("direccion"));
 		// Show the Up button in the action bar.
 		setupActionBar();
 
--- client/SimpleChat/src/org/ejmc/android/simplechat/LoginActivity.java
+++ client/SimpleChat/src/org/ejmc/android/simplechat/LoginActivity.java
@@ -1,8 +1,12 @@
 package org.ejmc.android.simplechat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Main activity.
@@ -14,10 +18,17 @@ import android.view.Menu;
  */
 public class LoginActivity extends Activity {
 
+	private Button btnEntrar;
+	private EditText nombre, ip;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		nombre = (EditText) findViewById(R.id.txtNombre);
+		ip = (EditText) findViewById(R.id.txtIP);	
+		btnEntrar = (Button) findViewById(R.id.btnEntrar);
 	}
 
 	@Override
@@ -26,5 +37,11 @@ public class LoginActivity extends Activity {
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
+	
+	public void ejecutar(View view) {
+        Intent i = new Intent(this, ChatActivity.class);
+        i.putExtra("direccion", ip.getText().toString());
+        startActivity(i);
+    }
 
 }
